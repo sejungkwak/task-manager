a1552e6683895e5a5182619a120c0dcff01be5c3
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-25 12:24:57 +0000

    Create custom CSS, JS

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidenav Initialisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
