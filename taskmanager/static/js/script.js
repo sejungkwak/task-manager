eb1c308d43bcdf799d9d943a3c3fbea1bc9061ee
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-25 16:20:00 +0000

    Add modal function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Modal Initialisation
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+});
